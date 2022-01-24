b7e638a4ab133e6775eb837d6921f77f061d04c4
David DeBruce <david.debruce@gmail.com>
2022-01-24 08:05:29 -0500

    submission

--- server/index.js
+++ server/index.js
@@ -2,50 +2,33 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 3042;
-const {
-	hexToBytes,
-	concatBytes,
-	toHex,
-	utf8ToBytes,
-} = require('ethereum-cryptography/utils');
 const EC = require('elliptic').ec;
-
 const ec = new EC('secp256k1');
+const SHA256 = require('crypto-js/sha256');
 
-const key1 = ec.genKeyPair();
-const key2 = ec.genKeyPair();
-const key3 = ec.genKeyPair();
+const balances = {
+	1: 100,
+	2: 50,
+	3: 75,
+};
 
-const publicKey1 = key1.getPublic().encode('hex');
-const publicKey2 = key2.getPublic().encode('hex');
-const publicKey3 = key3.getPublic().encode('hex');
+for (let i = 0; i < 3; i++) {
+	const newKey = ec.genKeyPair();
+	let key = {
+		private: newKey.getPrivate().toString(16),
+		public: newKey.getPublic().encode('hex').toString(),
+	};
+	balances[key.public] = balances[(i + 1).toString()];
+	delete balances[(i + 1).toString()];
+	console.log(key);
+}
 
-const privateKey1 = key1.getPrivate().toString(16);
-const privateKey2 = key2.getPrivate().toString(16);
-const privateKey3 = key3.getPrivate().toString(16);
+console.log(balances);
 
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
-
-const balances = {};
-
-balances[publicKey1] = 100;
-balances[publicKey2] = 50;
-balances[publicKey3] = 75;
-
-const privateKeys = {
-	privateKey1,
-	privateKey2,
-	privateKey3,
-};
-
-const publicKeys = {
-	publicKey1,
-	publicKey2,
-	publicKey3,
-};
 
 app.get('/balance/:address', (req, res) => {
 	const { address } = req.params;
@@ -53,18 +36,30 @@ app.get('/balance/:address', (req, res) => {
 	res.send({ balance });
 });
 
-app.post('/send', async (req, res) => {
-	const { sender, recipient, amount } = req.body;
-	balances[sender] -= amount;
-	balances[recipient] = (balances[recipient] || 0) + +amount;
-	res.send({ balance: balances[sender] });
+app.post('/send', (req, res) => {
+	const { sender, recipient, amount, privateKey } = req.body;
+	if (privateKey && recipient && amount && sender) {
+		let enteredKey = ec.keyFromPrivate(privateKey, 'hex');
+		let msgHash = SHA256(sender + recipient + amount);
+		let signature = enteredKey.sign(msgHash.toString());
+		if (
+			ec
+				.keyFromPublic(sender, 'hex')
+				.verify(msgHash.toString(), signature) &&
+			privateKey
+		) {
+			balances[sender] -= amount;
+			balances[recipient] = (balances[recipient] || 0) + +amount;
+			res.send({ balance: balances[sender] });
+			console.log('Key Verified');
+		} else {
+			console.log('Invalid Key');
+		}
+	} else {
+		console.log('Please complete all fields');
+	}
 });
 
 app.listen(port, () => {
 	console.log(`Listening on port ${port}!`);
-	console.log(balances);
-	console.log('Public Keys');
-	console.log(publicKeys);
-	console.log('Private Keys');
-	console.log(privateKeys);
 });
